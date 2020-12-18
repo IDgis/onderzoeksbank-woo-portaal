27030d9b527852c08f49512fdca099fb8192940f
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2020-12-18 15:26:05 +0100

    added a default about page

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import withTabs from '../main/withTabs';
+
+const About = () => (
+    <div className="content">
+        <p>
+            De informatie op deze pagina volgt nog ...
+        </p>
+    </div>
+);
+
+export default withTabs(About);
