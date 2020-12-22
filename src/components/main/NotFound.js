fe6f948bc98aed7bad8d85ae94c3cc935ffc6751
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2020-12-22 11:06:25 +0100

    updated not found page

--- src/components/main/NotFound.js
+++ src/components/main/NotFound.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const NotFound = ({setActiveTab}) => (
-    <div className="not_found">
-        <h1>404: Not Found</h1>
-    </div>
-);
+const NotFound = ({setActiveTab}) => {
+
+    useEffect(() => {
+        setActiveTab("");
+    }, []);
+
+    return (
+        <div id="content" className="content">
+            <div className="dummy">
+                <div className="content_main">
+                    <h1>404: Not Found</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default NotFound;
