1821fda03fb184f10e34b16fefb8a2e3ff6af423
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2021-01-26 11:09:25 +0100

    updated development url

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-REACT_APP_API_HOST="https://test-admin.geoportaal.local" REACT_APP_DEPLOYMENT_ACCEPTANCE="false" npm start
+REACT_APP_API_HOST="https://admin.geoportaal.local" REACT_APP_DEPLOYMENT_ACCEPTANCE="false" npm start
