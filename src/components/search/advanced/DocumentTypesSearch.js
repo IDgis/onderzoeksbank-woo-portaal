544c3a2fbe807764f4914a3968785ee04b502a86
Sandro Neumann <sandro.neumann@idgis.nl>
2024-01-26 15:54:03 +0000

    Fix typo

--- src/components/search/advanced/DocumentTypesSearch.js
+++ src/components/search/advanced/DocumentTypesSearch.js
@@ -5,7 +5,7 @@ const DocumentTypesSearch = forwardRef(({documentTypes}, documentTypeRef) => (
         <td>
             <label htmlFor="type-documents">Type documenten</label>
             <select id="type-documents" className="zoekselect" name="documentTypes" ref={documentTypeRef}>
-                <option value="all">- Alle typen document -</option>
+                <option value="all">- Alle type documenten -</option>
                 {
                     documentTypes?.map(documentType =>
                         <option key={documentType.id} value={documentType.id}>{ documentType.label }</option>
